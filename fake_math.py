24a5b69943a592847faa83a01300ecaaf91266a9
Павел Сотченков <dr.sot4enkoff2017@yandex.ru>
2024-07-08 19:09:29 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        return 'Критическая  ошибка'
+    else:
+        result = first / second
+        return result
---
+++ module 4 1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as dv1
+from true_math import divide as dv2
+
+result_1 = dv1(39, 3)
+result_2 = dv1(7, 0)
+result_3 = dv2(66, 2)
+result_4 = dv2(44, 0)
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
